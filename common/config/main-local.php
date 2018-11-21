e5ca8afd7ec5590c46446f8c2957ae295900b06c
sergey <sergtrade@gmail.com>
2018-11-21 15:46:16 +0200

    delete 1

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=dbprosfera',
             'username' => 'root',
-            'password' => '1',
+            'password' => '',
             'charset' => 'utf8',
         ],
         'mailer' => [
